9f87d362ce82a720dfa2a48f427ede5a668b1b6a
Leku Genuine Tita <titaleku003@gmail.com>
2025-03-04 16:37:10 +0100

    Add weather data retrieval script using OpenWeatherMap API

---
+++ testWeather.py
@@ -0,0 +1,19 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('API_KEY')
+city = "Buea"
+URL = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}' #change weather to forecast for 5 day forecast
+
+response = requests.get(URL)
+data = response.json()
+
+if response.status_code == 200:
+    print(f"Temperature: {data['main']['temp']}C")
+    print(f"Humidity: {data['main']['humidity']}%")
+    print(f"Pressure: {data['main']['pressure']}hPa")
+    print(f"Wind Speed: {data['wind']['speed']}m/s")
+else:
+    print("Error in API request")
