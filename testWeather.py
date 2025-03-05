7ebb62bb1dcc87c5bd721f0b0dbbea1741527f16
Leku Genuine Tita <titaleku003@gmail.com>
2025-03-05 01:08:27 +0100

    Refactor weather data retrieval to return structured data and handle exceptions

--- testWeather.py
+++ testWeather.py
@@ -6,16 +6,22 @@ load_dotenv()
 
 def get_weather(city):
     token = os.getenv('API_KEY')
-    city = "Buea"
     URL = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}' #change weather to forecast for 5 day forecast
-
-    response = requests.get(URL)
-    data = response.json()
-
-    if response.status_code == 200:
-        print(f"Temperature: {data['main']['temp']}C")
-        print(f"Humidity: {data['main']['humidity']}%")
-        print(f"Pressure: {data['main']['pressure']}hPa")
-        print(f"Wind Speed: {data['wind']['speed']}m/s")
-    else:
-        print("Error in API request")
+    
+    try:
+        response = requests.get(URL)
+        data = response.json()
+        if response.status_code == 200:
+            weather_data = {
+                'city': data['name'],
+                'temperature': data['main']['temp'],
+                'humidity': data['main']['humidity'],
+                'pressure': data['main']['pressure'],
+                'wind_speed': data['wind']['speed']
+            }
+            return weather_data
+        else:
+            return None
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
