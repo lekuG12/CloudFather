8ab28bc019a0e8f909d8078720ac9be2f8a9ee27
Leku Genuine Tita <titaleku003@gmail.com>
2025-03-05 01:01:38 +0100

    Add weather information handling to bot with city command

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 from dotenv import load_dotenv
+from testWeather import get_weather
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -8,10 +9,15 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! I am a CLOUDFATHER.")
+    bot.reply_to(message, "Hello! I am a CLOUDFATHER\n\nEnter the CITY you want weather information for")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['City'])
+def city_handler(message):
+    city = message.text
+    weather_info = get_weather(city)
+    text = 'Getting weather information for' + city
+    sent_msg = bot.reply_to(message.chat.id, text, parse_mode='Markdown')
+    
 
 bot.infinity_polling()
--- testWeather.py
+++ testWeather.py
@@ -3,17 +3,19 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-token = os.getenv('API_KEY')
-city = "Buea"
-URL = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}' #change weather to forecast for 5 day forecast
 
-response = requests.get(URL)
-data = response.json()
+def get_weather(city):
+    token = os.getenv('API_KEY')
+    city = "Buea"
+    URL = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}' #change weather to forecast for 5 day forecast
 
-if response.status_code == 200:
-    print(f"Temperature: {data['main']['temp']}C")
-    print(f"Humidity: {data['main']['humidity']}%")
-    print(f"Pressure: {data['main']['pressure']}hPa")
-    print(f"Wind Speed: {data['wind']['speed']}m/s")
-else:
-    print("Error in API request")
+    response = requests.get(URL)
+    data = response.json()
+
+    if response.status_code == 200:
+        print(f"Temperature: {data['main']['temp']}C")
+        print(f"Humidity: {data['main']['humidity']}%")
+        print(f"Pressure: {data['main']['pressure']}hPa")
+        print(f"Wind Speed: {data['wind']['speed']}m/s")
+    else:
+        print("Error in API request")
